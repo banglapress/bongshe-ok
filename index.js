8f0645ef7f82c9013b36481a9007f27b01079a63
banglapress <zafar21@gmail.com>
2021-12-23 02:11:34 +0600

    products API updated

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ async function run() {
         })
 
 
-        app.get('/products/:id', async (req, res) => {
+        app.get('/products/:productId', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productsCollection.findOne(query);
